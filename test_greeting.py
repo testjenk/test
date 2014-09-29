be346835144b819d20b218544451937ffd940ee9
Ha <ha@localhost>
2014-09-28 21:46:44 -0700

    add more test

--- test_greeting.py
+++ test_greeting.py
@@ -1,5 +1,6 @@
 from selenium import webdriver
 driver = webdriver.Firefox()
-driver.get("http://192.168.1.192")
-my_text = driver.find_element_by_id("greeting")
-print my_text
+def test_greeting():
+	driver.get("http://192.168.1.192")
+	my_text = driver.find_element_by_id("greeting")
+	assert my_text == "Welcome to Ha's Home"
